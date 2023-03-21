12b27450aae5ccb8b62f4c65e508d228c6c618fe
Nicolas Castro <nicolas.y0909@gmail.com>
2023-03-21 14:17:13 -0300

    fix: add dotenv [fixed]

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,11 +4,11 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from 'config';
 import { JwtStrategy } from './jwt.strategy';
 import { JwtService } from '@nestjs/jwt';
 import { ShopkeeperService } from 'module/shopkeeper/services';
 import { PrismaService } from 'module/prisma';
+import { jwtConstants } from '~/config/constants';
 
 @Module({
   imports: [
